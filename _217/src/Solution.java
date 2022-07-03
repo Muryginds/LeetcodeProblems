96d000a384b12365ffe8499c8d227c22924269eb
Muryginds <muryginds@gmail.com>
2022-07-03 13:46:59 +0300

    #217 solved

---
+++ _217/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+
+  public static void main(String[] args) {
+    int[] testArray1 = {1,2,3,1};
+    int[] testArray2 = {1,2,3,4};
+    int[] testArray3 = {1,1,1,3,3,4,3,2,4,2};
+    Solution solution = new Solution();
+    System.out.println(solution.containsDuplicate(testArray1));
+    System.out.println(solution.containsDuplicate(testArray2));
+    System.out.println(solution.containsDuplicate(testArray3));
+  }
+
+  public boolean containsDuplicate(int[] nums) {
+    Set<Integer> numbers = new HashSet<>();
+    for (int num : nums) {
+      if (numbers.contains(num)) {
+        return true;
+      }
+      numbers.add(num);
+    }
+    return false;
+  }
+}
